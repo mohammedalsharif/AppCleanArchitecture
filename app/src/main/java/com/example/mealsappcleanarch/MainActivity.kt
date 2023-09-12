a49b2a4423a427dfb09d5c85fb29cba366fede06
Mohammed al sharif <ffmm972006@gmail.com>
2023-09-12 15:09:22 +0300

    my test conflict

--- app/src/main/java/com/example/mealsappcleanarch/MainActivity.kt
+++ app/src/main/java/com/example/mealsappcleanarch/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
         val categoryAdapter = CategoryAdapter()
 
-        viewModel.getMeals()
+        viewModel.toString()
 
         lifecycleScope.launch {
             viewModel.meals.collect {result->
